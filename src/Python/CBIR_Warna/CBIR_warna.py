7f2a09198eb52d8c00139483d11e47d198d30968
Onta <mdz54dzakiarta@gmail.com>
2023-11-06 01:26:34 +0700

    fix: denormalisasiWarna, Hue, Saturation, Value, img_to_array_RGB, img_to_Matrix_HSV, cosineSim.

--- src/Python/CBIR_Warna/CBIR_warna.py
+++ src/Python/CBIR_Warna/CBIR_warna.py
@@ -1,56 +1,43 @@
 import numpy as np
 import cv2
 
-def denormalisasiWarna(list_RGB):
-    R_d = (list_RGB[0])/255
-    G_d = (list_RGB[1])/255
-    B_d = (list_RGB[2])/255
-    hasil = [R_d, G_d, B_d]
+def denormalisasiWarna(Array_RGB):
+    hasil = Array_RGB / 255
     return hasil
 
-def Hue(List_Denorm_RBG):
-    R_d = List_Denorm_RBG[0]
-    G_d = List_Denorm_RBG[1]
-    B_d = List_Denorm_RBG[2]
-    C_max = max(R_d,G_d,B_d)
-    C_min = min(R_d.G_d,B_d)
+def Hue(R_d, G_d, B_d):
+    C_max = max(R_d, G_d, B_d)
+    C_min = min(R_d, G_d, B_d)
     delta = C_max - C_min
 
     # Menghitung nilai H dalam derajat
-    if (C_max == R_d):
-        H = 60 * (((G_d - B_d)/delta) % 6)
-        return H
-    elif(C_max == G_d):
-        H = 60 * (((B_d - R_d)/delta) + 2)
-        return H
-    elif(C_max == B_d):
-        H = 60 * (((R_d - G_d)/delta) + 4)
-        return H
+    if (delta == 0):
+        return 0
+    elif C_max == R_d:
+        H = 60 * (((G_d - B_d) / delta) % 6)
+    elif C_max == G_d:
+        H = 60 * (((B_d - R_d) / delta) + 2)
+    elif C_max == B_d:
+        H = 60 * (((R_d - G_d) / delta) + 4)
+    return H
 
-def Saturation(List_denorm_RGB):
-    R_d = List_Denorm_RBG[0]
-    G_d = List_Denorm_RBG[1]
-    B_d = List_Denorm_RBG[2]
-    C_max = max(R_d,G_d,B_d)
-    C_min = min(R_d.G_d,B_d)
+def Saturation(R_d, G_d, B_d):
+    C_max = max(R_d, G_d, B_d)
+    C_min = min(R_d, G_d, B_d)
     delta = C_max - C_min
 
     # Menghitung nilai H dalam derajat
-    
-    if (C_max == 0):
+    if C_max == 0:
         return 0
     else:
-        S = delta/C_max
+        S = delta / C_max
         return S
 
-def Value(List_denorm_RGB):
-    R_d = List_Denorm_RBG[0]
-    G_d = List_Denorm_RBG[1]
-    B_d = List_Denorm_RBG[2]
-    C_max = max(R_d,G_d,B_d)
+def Value(R_d, G_d, B_d):
+    C_max = max(R_d, G_d, B_d)
     return C_max
 
-def img_to_rgb_list(image_path):
+def img_to_array_RGB(image_path):
     # Baca gambar menggunakan OpenCV
     img = cv2.imread(image_path)
 
@@ -60,31 +47,36 @@ def img_to_rgb_list(image_path):
     # Dapatkan matriks piksel RGB
     list_RGB = img_rgb.tolist()
 
-    return list_RGB
+    # Matrix RGB
+    Matrix_RGB = np.array(list_RGB)
 
-def cosineSim(vector1, vector2):
-    dotProduct = np.dot(vector1, vector2)
-    A = np.sqrt(np.sum(vector1 ** 2))
-    B = np.sqrt(np.sum(vector2 ** 2))
-    return dotProduct / (A * B)
+    return Matrix_RGB
 
-def img_to_VektorHSV(img_path):
-    # Mengubah img ke RGB vektor
-    List_RGB = img_to_rgb_list(img_path)
+def img_to_Matrix_HSV(img_path):
+    # Mengubah img ke list RGB
+    List_RGB = img_to_array_RGB(img_path)
 
     # Menghitung List_Denorm_RGB
-    List_Denorm_RGB = denormalisasiWarna(List_RGB)
+    array_Denorm_RGB = denormalisasiWarna(List_RGB)
 
-    # Menghitung Hue
-    H = Hue(List_Denorm_RGB)
+    # Menghitung matriks HSV secara vektorisasi
+    Matrix_HSV = np.array([(Hue(*pixel), Saturation(*pixel), Value(*pixel)) for row in array_Denorm_RGB for pixel in row])
 
-    # Menghitung Saturation
-    S = Saturation(List_Denorm_RGB)
+    return Matrix_HSV
 
-    # Menghitung Value
-    V = Value(List_Denorm_RGB)
+def cosineSim(Matrix1, Matrix2):
+    dotProduct = np.sum(np.dot(Matrix1[i], Matrix2[i]) for i in range(Matrix1.shape[0]))
+    A = np.sqrt(np.sum(np.fromiter((np.sum(Matrix1[i] ** 2) for i in range(Matrix1.shape[0])), dtype=np.float64)))
+    B = np.sqrt(np.sum(np.fromiter((np.sum(Matrix2[i] ** 2) for i in range(Matrix2.shape[0])), dtype=np.float64)))
+    return dotProduct / (A * B) if (A * B) != 0 else np.nan
 
-    # Menyusun vektor HSV
-    Vektor_HSV = np.array([H,S,V])
+def CBIR_Warna(img_path_1, img_path_2):
+    Matrix1 = img_to_Matrix_HSV(img_path_1)
+    Matrix2 = img_to_Matrix_HSV(img_path_2)
 
-    return Vektor_HSV
+    # Cari CBIR dengan cosineSim
+    similarity = cosineSim(Matrix1, Matrix2)
+
+    # Mengubah nilai similarity menjadi persen
+    similarity = round(similarity * 100)
+    return similarity
