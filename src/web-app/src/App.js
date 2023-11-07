d836a69e1f73aacf19b911af1d00d8019329cc08
Albert Ghazaly <albert260306@gmail.com>
2023-11-07 08:22:40 +0700

    feat: create web layout

--- src/web-app/src/App.js
+++ src/web-app/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './ImageUpload'
+import ImageUpload from './ImageUpload';
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <div className="HeadTab">
+          <button id="ButtonHead1"> About us</button>
+          <button id="ButtonHead2">camera</button>
+          <button id="ButtonHead3">help</button>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="Title">Sistem Temu Balik Gambar</h1>
+        <div class="boxInputImage">
+          <p>ceritanya gambar input</p>
+        </div>
+       <div class="boxResult">
+          <p>hasil</p>
+       </div>
       </header>
     </div>
   );
