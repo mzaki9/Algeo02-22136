1b7e588c079a8ec3dd0b753e3464f6f4e4a40522
Muhammad Zaki <muhammadzaki9@ymail.com>
2023-11-09 23:19:54 +0700

    feat add compress image

--- src/Python/CBIR_Texture/CBIR_Texture.py
+++ src/Python/CBIR_Texture/CBIR_Texture.py
@@ -3,11 +3,21 @@ import numpy as np
 import time
 import os
 
+def compress_image(image, compression_scale=0.75, quality=85):
+    # Mengkompresi gambar dengan skala tertentu
+    compressed_image = cv2.resize(image, (0, 0), fx=compression_scale, fy=compression_scale)
+    _, compressed_image_data = cv2.imencode('.jpg', compressed_image, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
+    return compressed_image_data
 
 def cbirTexture(image):
-    # Hitung gambar grayscale dengan rumus yang diberikan
+    
+    # Kompresi gambar sebelum diproses
+    compressed_image_data = compress_image(image, compression_scale=0.7, quality=80)
 
-    greyImage = 0.29 * image[..., 2] + 0.587 * image[..., 1] + 0.114 * image[..., 0]
+    # Mendekompresi gambar
+    decompressed_image = cv2.imdecode(compressed_image_data, cv2.IMREAD_COLOR)
+
+    greyImage = 0.29 * decompressed_image[..., 2] + 0.587 * decompressed_image[..., 1] + 0.114 * decompressed_image[..., 0]
 
     greyImage = greyImage.astype(np.uint8)
 
@@ -40,27 +50,44 @@ def cbirTexture(image):
     return vector
 
 def calculateHomogenity(glcmNorm):
-    homogenity = 0
-    for i in range(glcmNorm.shape[0]):
-        for j in range(glcmNorm.shape[1]):
-            homogenity += glcmNorm[i, j] / (1 + (i - j) ** 2)
-    return homogenity
+    # homogenity = 0
+    # for i in range(glcmNorm.shape[0]):
+    #     for j in range(glcmNorm.shape[1]):
+    #         homogenity += glcmNorm[i, j] / (1 + (i - j) ** 2)
+
+    i, j = np.indices(glcmNorm.shape)
+
+    # Hitung homogeneity
+    homogeneity = np.sum(glcmNorm / (1. + (i - j) ** 2))
+
+    return homogeneity
 
 def CalculateEntropy(glcmNorm):
-    entropy = 0
-    for i in range(glcmNorm.shape[0]):
-        for j in range(glcmNorm.shape[1]):
-            #Agar tidak log 0
-            if glcmNorm[i, j] > 0:
-                entropy += glcmNorm[i, j] * np.log2(glcmNorm[i, j])
-    entropy = -entropy
+    # entropy = 0
+    # for i in range(glcmNorm.shape[0]):
+    #     for j in range(glcmNorm.shape[1]):
+    #         #Agar tidak log 0
+    #         if glcmNorm[i, j] > 0:
+    #             entropy += glcmNorm[i, j] * np.log2(glcmNorm[i, j])
+    # entropy = -entropy
+
+    positive_glcmNorm = glcmNorm[glcmNorm > 0]
+
+    # Hitung entropy
+    entropy = -np.sum(positive_glcmNorm * np.log2(positive_glcmNorm))
     return entropy
 
 def calculateContrast(glcmNorm):
-    contrast = 0
-    for i in range(glcmNorm.shape[0]):
-        for j in range(glcmNorm.shape[1]):
-            contrast += ((i - j) ** 2) * glcmNorm[i, j]
+    # contrast = 0
+    # for i in range(glcmNorm.shape[0]):
+    #     for j in range(glcmNorm.shape[1]):
+    #         contrast += ((i - j) ** 2) * glcmNorm[i, j]
+
+    i, j = np.indices(glcmNorm.shape)
+
+    # Hitung contrast
+    contrast = np.sum(((i - j) ** 2) * glcmNorm)
+    print(contrast)
     return contrast
 
 def cosineSim(vector1, vector2):
@@ -71,36 +98,24 @@ def cosineSim(vector1, vector2):
 
 
 def main():
-    reference_image_path = 'ISI REFERENSI'
-    reference_image = cv2.imread(reference_image_path)
-    reference_vector = cbirTexture(reference_image)
-
-    dataset_path = 'ISI PATH DATASET'
-    dataset_images = os.listdir(dataset_path)
-
-    similarity_scores = {}
-
+    path1 = 'ref 1'
+    path2 = 'ref 2'
+    image1 = cv2.imread(path1)
+    image2 = cv2.imread(path2)
     start_time = time.time()
+    vector1 = cbirTexture(image1)
+    vector2 = cbirTexture(image2)
+    hasil = cosineSim(vector1, vector2)
+    persentase_kesamaan = (hasil)*100
 
-    for image_name in dataset_images:
-        image_path = os.path.join(dataset_path, image_name)
-        image = cv2.imread(image_path)
-        image_vector = cbirTexture(image)
-        similarity_score = cosineSim(reference_vector, image_vector)
-        similarity_scores[image_name] = similarity_score
-
+    # Print persentase kesamaan
+    print("Persentase Kesamaan (dalam persen):", persentase_kesamaan)
     end_time = time.time()
-
-    execution_time = end_time - start_time
-    print("Execution Time: {:.2f} seconds".format(execution_time))
-
-
-    sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
-    for image_name, similarity_score in sorted_similarity_scores:
-        print(f"Image: {image_name}, Similarity: {similarity_score * 100:.2f}%")
+    print("Execution Time: {:.2f} seconds".format(end_time - start_time))  
 
 if __name__ == "__main__":
     main()
 
     
+
 
