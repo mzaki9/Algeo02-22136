f7b384205a5bf8170b120886a0f2d5105a7f8aaf
Onta <mdz54dzakiarta@gmail.com>
2023-11-12 22:18:21 +0700

    fix: cosine similarity

--- src/Python/CBIR_Warna/CBIR_warna.py
+++ src/Python/CBIR_Warna/CBIR_warna.py
@@ -3,27 +3,22 @@ import cv2
 import time
 
 def img_to_matrix_RGB(image_path):
-    # Baca gambar menggunakan OpenCV
     img = cv2.imread(image_path)
-
-    # Ubah format warna dari BGR ke RGB
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Konversi ke tipe data float
-    img_rgb = img_rgb.astype(np.float32)
-
-    img_rgb /= 255.0
+    # Denormalisasi warna
+    img_rgb = img_rgb.astype(np.float32)/255.0
     return img_rgb
 
 def matrix_rgb_to_hist(image):
-    # Membagi matrix image menjadi 3 matrix representasi rred, green, blue
+
+    # Membagi matrix image menjadi 3 matrix representasi red, green, blue
     r,g,b = image[:,:,0], image[:,:,1], image[:,:,2]
 
     # Mencari cmax, cmin, delta
     cmax = np.max(image, axis=2)
     cmin = np.min(image, axis=2)
-    delta = (cmax-cmin)
-
+    delta = np.subtract(cmax,cmin)
     # Inisialisasi matrix h,s,v
     h = np.zeros_like(r)
     s = np.zeros_like(r)
@@ -37,90 +32,54 @@ def matrix_rgb_to_hist(image):
     # Menghitung nilai h
     h[delta == 0] = 0
     h[mask_r] = 60*(((g[mask_r] -b[mask_r])/(delta[mask_r]))%6)
-    h[mask_g] = 60*(((g[mask_g] -b[mask_g])/(delta[mask_g]))+2)
-    h[mask_b] = 60*(((g[mask_b] -b[mask_b])/(delta[mask_b]))+4)
+    h[mask_g] = 60*(((b[mask_g] -r[mask_g])/(delta[mask_g]))+2)
+    h[mask_b] = 60*(((r[mask_b] -g[mask_b])/(delta[mask_b]))+4)
 
     # Hitung nilai s
     s[cmax == 0] = 0
     s[cmax != 0] = (delta[cmax != 0]) / (cmax[cmax != 0])
 
-
     # Hitung nilai v
     v = np.maximum.reduce([r,g,b])
 
-    # Menggunakan procedure quantify
     quantify(h,s,v)
 
+    hist_h = cv2.calcHist([h], [0], None, [8], [0, 8]).flatten()
+    hist_s = cv2.calcHist([s], [0], None, [3], [0, 3]).flatten()
+    hist_v = cv2.calcHist([v], [0], None, [3], [0, 3]).flatten()
 
-    # Mencari histogram h,s,v
-    hist_h = cv2.calcHist([h],[0], None, [8], [0,7]).flatten()
-    hist_s = cv2.calcHist([s],[0], None, [3], [0,2]).flatten()
-    hist_v = cv2.calcHist([v],[0], None, [3], [0,2]).flatten()
-    # print("h")
-    # print(h)
-    # print("s")
-    # print(s)
-    # print("v")
-    # print(v)
-    # Menyatukan ke 3 histogram
-    hist = np.concatenate((hist_h,hist_s,hist_v), axis=0)
-
-    return hist
+    return np.concatenate((hist_h, hist_s, hist_v))
 
 def quantify(h,s,v):
-    # Quantify h
-    h[h >= 316] = 0
-    h[np.logical_and(h >= 1, h <= 25)] = 1
-    h[np.logical_and(h >= 26, h <= 40)] = 2
-    h[np.logical_and(h >= 41, h <= 120)] = 3
-    h[np.logical_and(h >= 121, h <= 190)] = 4
-    h[np.logical_and(h >= 191, h <= 270)] = 5
-    h[np.logical_and(h >= 271, h <= 295)] = 6
-    h[np.logical_and(h >= 296, h <= 315)] = 7
+    h[h > 315] = 0
+    h[np.logical_and(h > 0, h <= 25)] = 1
+    h[np.logical_and(h > 25, h <= 40)] = 2
+    h[np.logical_and(h > 40, h <= 120)] = 3
+    h[np.logical_and(h > 120, h <= 190)] = 4
+    h[np.logical_and(h > 190, h <= 270)] = 5
+    h[np.logical_and(h > 270, h <= 295)] = 6
+    h[np.logical_and(h > 295, h <= 315)] = 7
 
     # Quantify s
-    s[s < 0.2] = 0
-    s[np.logical_and(s >= 0.2, s < 0.7)] = 1
     s[s >= 0.7] = 2
+    s[np.logical_and(s >= 0.2, s < 0.7)] = 1
+    s[s < 0.2] = 0
 
     # Quantify v
-    v[v < 0.2] = 0
-    v[np.logical_and(v >= 0.2, v < 0.7)] = 1
     v[v >= 0.7] = 2
+    v[np.logical_and(v >= 0.2, v < 0.7)] = 1
+    v[v < 0.2] = 0
 
 def create_submatrices(matrix):
-    len_rows, len_cols, _ = matrix.shape
-
-    # Tentukan batas indeks untuk pembagian
-    row_splits = [0, len_rows // 3, 2 * len_rows // 3, len_rows]
-    col_splits = [0, len_cols // 3, 2 * len_cols // 3, len_cols]
-
-    # Inisialisasi daftar untuk menyimpan submatriks
-    submatrices = []
-
-    # Loop melalui bagian-bagian matriks dan potong sesuai batas indeks
-    for i in range(3):
-        for j in range(3):
-            start_row, end_row = row_splits[i], row_splits[i + 1]
-            start_col, end_col = col_splits[j], col_splits[j + 1]
-
-            submatrix = matrix[start_row:end_row, start_col:end_col]
-            submatrices.append(submatrix)
-
-    return submatrices
+    return [matrix[i:i+matrix.shape[0]//4, j:j+matrix.shape[1]//4]
+            for i in range(0, matrix.shape[0], matrix.shape[0]//4)
+            for j in range(0, matrix.shape[1], matrix.shape[1]//4)]
 
 def cosinesim(vector_A, vector_B):
-    # Hitung dot product antara dua vektor
     dot_product = np.dot(vector_A, vector_B)
-
-    # Hitung norma Euclidean dari masing-masing vektor
     norm_A = np.linalg.norm(vector_A)
     norm_B = np.linalg.norm(vector_B)
-
-    # Hitung cosine similarity
-    similarity = dot_product / (norm_A * norm_B)
-
-    return similarity
+    return dot_product / (norm_A * norm_B) 
 
 def CBIR_Warna(img_path_1, img_path_2):
     Matrix1 = img_to_matrix_RGB(img_path_1)
@@ -132,11 +91,10 @@ def CBIR_Warna(img_path_1, img_path_2):
     vektor1 = [matrix_rgb_to_hist(matrix) for matrix in m_matriks]
     vektor2 = [matrix_rgb_to_hist(matrix) for matrix in n_matriks]
 
-    similarity = [cosinesim(vektor1[i],vektor2[i]) for i in range(len(vektor1))]
+    similarity = [cosinesim(vektor1[i], vektor2[i]) for i in range(len(vektor1))]
 
-    # Mencari nilai rata rata dari cosine Similarity
-    result = sum(similarity) / len(similarity) * 100
-    return round(result)
+    result = sum(similarity) / len(similarity)
+    return result
 
 def main():
     path1 = 'ref 1'
@@ -147,6 +105,10 @@ def main():
     persentase_kesamaan = CBIR_Warna(path1,path2)
 
      # Print persentase kesamaan
-    print("Persentase Kesamaan (dalam persen):", persentase_kesamaan)
+    print("Persentase Kesamaan (dalam persen):", persentase_kesamaan*100)
     end_time = time.time()
     print("Execution Time: {:.2f} seconds".format(end_time - start_time))
+
+
+if __name__ == "__main__":
+    main()
--- src/Python/CBIR_Warna/testMultiprocessingWarna.py
+++ src/Python/CBIR_Warna/testMultiprocessingWarna.py
@@ -5,16 +5,11 @@ import os
 from multiprocessing import Pool
 
 def img_to_matrix_RGB(image_path):
-    # Baca gambar menggunakan OpenCV
     img = cv2.imread(image_path)
-
-    # Ubah format warna dari BGR ke RGB
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # Konversi ke tipe data float
     img_rgb = img_rgb.astype(np.float32)
-
-    img_rgb /= 255.0
+    img_rgb = np.array(img_rgb)
+    img_rgb = np.divide(img_rgb,255.0)
     return img_rgb
 
 def matrix_rgb_to_hist(image):
@@ -24,8 +19,7 @@ def matrix_rgb_to_hist(image):
     # Mencari cmax, cmin, delta
     cmax = np.max(image, axis=2)
     cmin = np.min(image, axis=2)
-    delta = (cmax-cmin)
-
+    delta = np.subtract(cmax,cmin)
     # Inisialisasi matrix h,s,v
     h = np.zeros_like(r)
     s = np.zeros_like(r)
@@ -39,82 +33,60 @@ def matrix_rgb_to_hist(image):
     # Menghitung nilai h
     h[delta == 0] = 0
     h[mask_r] = 60*(((g[mask_r] -b[mask_r])/(delta[mask_r]))%6)
-    h[mask_g] = 60*(((g[mask_g] -b[mask_g])/(delta[mask_g]))+2)
-    h[mask_b] = 60*(((g[mask_b] -b[mask_b])/(delta[mask_b]))+4)
+    h[mask_g] = 60*(((b[mask_g] -r[mask_g])/(delta[mask_g]))+2)
+    h[mask_b] = 60*(((r[mask_b] -g[mask_b])/(delta[mask_b]))+4)
 
     # Hitung nilai s
     s[cmax == 0] = 0
     s[cmax != 0] = (delta[cmax != 0]) / (cmax[cmax != 0])
 
-
     # Hitung nilai v
     v = np.maximum.reduce([r,g,b])
 
-    # Menggunakan procedure quantify
     quantify(h,s,v)
 
-    # Mencari histogram h,s,v
-    hist_h = cv2.calcHist([h],[0], None, [8], [0,7]).flatten()
-    hist_s = cv2.calcHist([s],[0], None, [3], [0,2]).flatten()
-    hist_v = cv2.calcHist([v],[0], None, [3], [0,2]).flatten()
-    hist = np.concatenate((hist_h,hist_s,hist_v), axis=0)
+    h = h.astype(np.uint8)
+    s = s.astype(np.uint8)
+    v = v.astype(np.uint8)
 
-    return hist
+    hist_h = cv2.calcHist([h], [0], None, [8], [0, 8]).flatten()
+    hist_s = cv2.calcHist([s], [0], None, [3], [0, 3]).flatten()
+    hist_v = cv2.calcHist([v], [0], None, [3], [0, 3]).flatten()
+
+    return np.concatenate((hist_h, hist_s, hist_v))
 
 def quantify(h,s,v):
     # Quantify h
-    h[h >= 316] = 0
-    h[np.logical_and(h >= 1, h <= 25)] = 1
-    h[np.logical_and(h >= 26, h <= 40)] = 2
-    h[np.logical_and(h >= 41, h <= 120)] = 3
-    h[np.logical_and(h >= 121, h <= 190)] = 4
-    h[np.logical_and(h >= 191, h <= 270)] = 5
-    h[np.logical_and(h >= 271, h <= 295)] = 6
-    h[np.logical_and(h >= 296, h <= 315)] = 7
+    h[h > 315] = 0
+    h[np.logical_and(h > 0, h <= 25)] = 1
+    h[np.logical_and(h > 25, h <= 40)] = 2
+    h[np.logical_and(h > 40, h <= 120)] = 3
+    h[np.logical_and(h > 120, h <= 190)] = 4
+    h[np.logical_and(h > 190, h <= 270)] = 5
+    h[np.logical_and(h > 270, h <= 295)] = 6
+    h[np.logical_and(h > 295, h <= 315)] = 7
 
     # Quantify s
-    s[s < 0.2] = 0
-    s[np.logical_and(s >= 0.2, s < 0.7)] = 1
     s[s >= 0.7] = 2
+    s[np.logical_and(s >= 0.2, s < 0.7)] = 1
+    s[s < 0.2] = 0
 
     # Quantify v
-    v[v < 0.2] = 0
-    v[np.logical_and(v >= 0.2, v < 0.7)] = 1
     v[v >= 0.7] = 2
+    v[np.logical_and(v >= 0.2, v < 0.7)] = 1
+    v[v < 0.2] = 0
+
 
 def create_submatrices(matrix):
-    len_rows, len_cols, _ = matrix.shape
-
-    # Tentukan batas indeks untuk pembagian
-    row_splits = [0, len_rows // 3, 2 * len_rows // 3, len_rows]
-    col_splits = [0, len_cols // 3, 2 * len_cols // 3, len_cols]
-
-    # Inisialisasi daftar untuk menyimpan submatriks
-    submatrices = []
-
-    # Loop melalui bagian-bagian matriks dan potong sesuai batas indeks
-    for i in range(3):
-        for j in range(3):
-            start_row, end_row = row_splits[i], row_splits[i + 1]
-            start_col, end_col = col_splits[j], col_splits[j + 1]
-
-            submatrix = matrix[start_row:end_row, start_col:end_col]
-            submatrices.append(submatrix)
-
-    return submatrices
+    return [matrix[i:i+matrix.shape[0]//4, j:j+matrix.shape[1]//4]
+            for i in range(0, matrix.shape[0], matrix.shape[0]//4)
+            for j in range(0, matrix.shape[1], matrix.shape[1]//4)]
 
 def cosinesim(vector_A, vector_B):
-    # Hitung dot product antara dua vektor
     dot_product = np.dot(vector_A, vector_B)
-
-    # Hitung norma Euclidean dari masing-masing vektor
     norm_A = np.linalg.norm(vector_A)
     norm_B = np.linalg.norm(vector_B)
-
-    # Hitung cosine similarity
-    similarity = dot_product / (norm_A * norm_B)
-
-    return similarity
+    return dot_product / (norm_A * norm_B) if norm_A * norm_B != 0 else 0
 
 def CBIR_Warna(img_path_1, img_path_2):
     Matrix1 = img_to_matrix_RGB(img_path_1)
@@ -126,10 +98,9 @@ def CBIR_Warna(img_path_1, img_path_2):
     vektor1 = [matrix_rgb_to_hist(matrix) for matrix in m_matriks]
     vektor2 = [matrix_rgb_to_hist(matrix) for matrix in n_matriks]
 
-    similarity = [cosinesim(vektor1[i],vektor2[i]) for i in range(len(vektor1))]
+    similarity = [cosinesim(vektor1[i], vektor2[i]) for i in range(len(vektor1))]
 
-    # Mencari nilai rata rata dari cosine Similarity
-    result = sum(similarity) / len(similarity)
+    result = sum(similarity) / len(similarity) if len(similarity) != 0 else 0
     return result
 
 def process_image(args):
@@ -138,19 +109,19 @@ def process_image(args):
     matrix = img_to_matrix_RGB(image_path)
     n_matrix = create_submatrices(matrix)
     vektor = [matrix_rgb_to_hist(matrix) for matrix in n_matrix]
-    
-    similarity = [cosinesim(vektor_reference[i],vektor[i]) for i in range(len(vektor_reference))]
-    similarity_score = sum(similarity) / len(similarity)
+
+    similarity = [cosinesim(vektor_reference[i], vektor[i]) for i in range(len(vektor_reference))]
+    similarity_score = sum(similarity) / len(similarity) if len(similarity) != 0 else 0
 
     return (image_name, similarity_score)
 
 def main():
-    reference_image_path = 'ISI REFERENSI IMAGE'
+    reference_image_path = 'PATH GAMBAR QUERY'
     matrix_reference = img_to_matrix_RGB(reference_image_path)
     m_matrix_reference = create_submatrices(matrix_reference)
     vektor_reference = [matrix_rgb_to_hist(matrix) for matrix in m_matrix_reference]
 
-    dataset_path = 'NAMA FOLDER DATASET'
+    dataset_path = 'PATH DATASET'
     dataset_images = os.listdir(dataset_path)
 
     start_time = time.time()  
@@ -163,7 +134,7 @@ def main():
     similarity_scores = {image_name: similarity_score for image_name, similarity_score in results}
 
 
-    sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
+    sorted_similarity_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=False)
     for image_name, similarity_score in sorted_similarity_scores:
         print(f"Image: {image_name}, Similarity: {similarity_score * 100:.2f}%")
 
