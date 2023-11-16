945ec4ebb9d4b7e4ee22f7eb09fdb77308014596
Albert Ghazaly <albert260306@gmail.com>
2023-11-16 12:29:10 +0700

    feat: display the result

--- src/web-app/src/search.js
+++ src/web-app/src/search.js
@@ -1,4 +1,7 @@
 import React, { useRef, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import './App.css'
+import './import.css'
 
 function Search(){
     const [SwitchVal, setSwitchVal] = useState(1); 
@@ -15,6 +18,8 @@ function Search(){
                 });
                 const jsonData = await response.json();
                 setJSonVal(jsonData);
+                setTimeExec(jSonVal.execution_time);
+                setTupleList(jSonVal.results);
                 console.log(jSonVal);
     
             }catch(error){
@@ -28,12 +33,16 @@ function Search(){
                 });
                 const jsonData = await response.json();
                 setJSonVal(jsonData);
+                setTimeExec(jSonVal.execution_time);
+                setTupleList(jSonVal.results);
                 console.log(jSonVal);
     
             }catch(error){
                 console.log('Error request to API',error)
             }
         }
+        <PaginatedItems itemsPerPage={3} items={tupleList}></PaginatedItems>
+
     };
     const handleSwitchClick = ()=>{ // switch CBIR mode
         setSwitchVal(SwitchVal*-1);
@@ -51,11 +60,90 @@ function Search(){
             <button  onClick={searchImg } style={{position:'relative',bottom:'33vh', fontSize:'6vh'}}>
                 SEARCH
             </button>
-            <h3 style={{position:'relative',left:'30vw',bottom:'32vh',color:'white',fontSize:'3vh'}}>
-              (time: Undefined yet)
+            <h3 style={{position:'relative',bottom:'42vh',color:'white',fontSize:'4vh', left:'35vw'}}>
+              time: {timeExec} s
             </h3>
 
         </div>
     );
 }
+function Items({ currentItems}) {
+    return (
+      <div>
+        {currentItems &&
+           currentItems.map((img,index) => (
+            <>
+            <img
+            key={index}
+            src={img.image_name}
+            style={{
+                position: 'relative',
+                bottom: '60vh',
+                minHeight: '200px',
+                minWidth: '200px',
+                maxHeight: '280px',
+                maxWidth: '280px',
+                marginRight: '40px', // Add some space between images
+            }}
+            />
+            <p> {img.similarity_score}</p>
+            </>
+        )
+        )}
+      </div>
+    );
+  }
+
+function PaginatedItems({ itemsPerPage, items}) {
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+  
+    // Simulate fetching items from another resources.
+    // (This could be items from props; or items loaded in a local state
+    // from an API endpoint with useEffect and useState)
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = items.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(items.length / itemsPerPage);
+  
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % items.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    };
+  
+    return (
+      <>
+        <Items currentItems={currentItems} />
+        <div style={{position:'absolute',top:'45vh',left:'0vw'}}>
+            <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={2}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< prev"
+
+                    containerClassName={'react-paginate'}
+                    activeClassName={'active'}
+                    pageClassName={'page-item'}
+                    previousClassName={'page-item'}
+                    nextClassName={'page-item'}
+                    breakClassName={'page-item'}
+                    pageLinkClassName={'page-link'}
+                    previousLinkClassName={'page-link'}
+                    nextLinkClassName={'page-link'}
+                    breakLinkClassName={'page-link'}
+                    renderOnZeroPageCount={null}
+                  />
+        </div>
+      </>
+    );
+  }
+
 export default Search;
